9e2214894e5f6f3fcde3505f65a08938b21c72e9
krishnapant13 <krishna555.pant@gmail.com>
2024-07-18 12:37:02 +0530

    changed backend url

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 // export const server = "http://localhost:8000/api/v2";
-export const server = "https://oebe-w9c8.onrender.com/api/v2";
-export const backend_url = "https://oebe-w9c8.onrender.com/";
+export const server = "https://tpbe.onrender.com/api/v2";
+export const backend_url = "https://tpbe.onrender.com/";
